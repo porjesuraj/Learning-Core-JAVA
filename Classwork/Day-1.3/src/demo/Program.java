40763351cf4e210600190bc95eb5fd0a0f6fd51a
porjesuraj <porjesuraj5@gmail.com>
2020-10-26 19:09:03 +0530

    day1 classwork 'demo 1.3   , convert string to int,float,double  in main 6 done '

--- Classwork/Day-1.3/src/demo/Program.java
+++ Classwork/Day-1.3/src/demo/Program.java
@@ -6,6 +6,30 @@ public class Program {
 	
 	public static void main(String[] args)
 	{
+		
+		String a = "10";
+		
+		int num1 = Integer.parseInt(a);
+		
+		String b = "20.1f";
+		
+		float num2 = Float.parseFloat(b);
+		
+		String c = "30.2d";
+		double num3 = Double.parseDouble(c);
+		
+		double result = num1 + num2 + num3;
+		
+		System.out.println("result : " + result);
+	}
+	
+	
+	
+	
+	
+	
+	public static void main5(String[] args)
+	{
 		String strNumber = "125";
 		//STRING msg = "hello";
 		
