b19cee062e0d32300be9bd6a8ff829e37f20fcd1
porjesuraj <porjesuraj5@gmail.com>
2020-10-26 18:59:56 +0530

    day1 classwork 'demo 1.3  print int ,by init and assign in main 2 and 3 done '

--- Classwork/Day-1.3/src/demo/Program.java
+++ Classwork/Day-1.3/src/demo/Program.java
@@ -3,7 +3,31 @@ package demo;
 public class Program {
 	
 	
+	
 	public static void main(String[] args)
+	{
+		int number;
+		
+		//number = 10;
+		System.out.println("Number : " + number);
+		
+	}
+	
+	
+	
+	public static void main2(String[] args)
+	{
+		int number = 10;
+		
+		System.out.println("Number : " + number);
+		
+	}
+	
+	
+	
+	
+	
+	public static void main1(String[] args)
 	{
 		
 		String str = "Hello dac";
