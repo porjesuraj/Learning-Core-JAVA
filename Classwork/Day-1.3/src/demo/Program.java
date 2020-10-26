352cd36e2cc701dca36a41db322d4565d45338ad
porjesuraj <porjesuraj5@gmail.com>
2020-10-26 19:11:51 +0530

    day1 classwork 'demo 1.3   ,use argument 'args' to take input and  convert it,i.e a string type to int,float,double  in main  done '

--- Classwork/Day-1.3/src/demo/Program.java
+++ Classwork/Day-1.3/src/demo/Program.java
@@ -3,8 +3,29 @@ package demo;
 import javax.print.DocFlavor.STRING;
 
 public class Program {
-	
 	public static void main(String[] args)
+	{
+		
+		
+		
+		int num1 = Integer.parseInt(args[0]);
+		
+		
+		
+		float num2 = Float.parseFloat(args[1]);
+		
+		
+		double num3 = Double.parseDouble(args[2]);
+		
+		double result = num1 + num2 + num3;
+		
+		System.out.println("result : " + result);
+	}
+	
+	
+	
+	
+	public static void main6(String[] args)
 	{
 		
 		String a = "10";
