55c732d5dc5e435ddeabfb836f6fe3433faa6535
porjesuraj <porjesuraj5@gmail.com>
2020-10-27 16:49:27 +0530

    classwork day2 'demo 1.2 and vscode demo, one main method per class demo implemented '

---
+++ Classwork/Day2_27_Oct/Day_2.2/src/test/Program.java
@@ -0,0 +1,30 @@
+package test;
+
+class A{
+	public static void main(String[] args)
+	{
+		System.out.println("In A Main");
+		
+
+		
+	}
+	
+}
+
+
+public class Program {
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("In Program Main");
+		
+
+		
+	}
+	
+	
+
+}
+
+
