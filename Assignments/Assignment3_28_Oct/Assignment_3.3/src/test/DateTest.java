5819acdb3078b6fc9062f717bcbe099d245202a0
porjesuraj <porjesuraj5@gmail.com>
2020-10-28 20:40:42 +0530

    Assignment 3.3 completed

---
+++ Assignments/Assignment3_28_Oct/Assignment_3.3/src/test/DateTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class DateTest {
+
+	
+	public void displayDate(Date date)
+	{
+		System.out.println("Date : " + date.getDay() + "/" + date.getMonth() + "/" + date.getYear() );
+	}
+}
---
+++ Assignments/Assignment3_28_Oct/Assignment_3.3/src/test/Program.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Date date = new Date(1, 2, 2020);
+		
+		DateTest test = new DateTest();
+		
+		test.displayDate(date);
+	}
+
+}
