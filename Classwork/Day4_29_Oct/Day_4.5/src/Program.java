8bf6ef0f777bdefb003b97675775d9e8e902eda9
porjesuraj <porjesuraj5@gmail.com>
2020-10-29 16:02:16 +0530

    classwork day4 'demo of default /global package in folder  day4.5  implemented'

---
+++ Classwork/Day4_29_Oct/Day_4.5/src/Program.java
@@ -0,0 +1,12 @@
+
+
+public class Program{
+
+    public static void main(String[] args) {
+       
+       p1.Complex c1 = new p1.Complex();
+       System.out.println(c1.toString()); 
+
+    }
+}
+
