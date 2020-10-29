cd3a90779fbfbd41f54c673d81ed4be6ce44648e
porjesuraj <porjesuraj5@gmail.com>
2020-10-29 17:00:08 +0530

    classwork day4 'demo  in folder  day4.10  implemented'

---
+++ Classwork/Day4_29_Oct/Day_4.10/src/Program.java
@@ -0,0 +1,13 @@
+
+package p1.p3;
+import p1.p2.Complex;
+//import p1.Complex;
+public class Program{
+
+    public static void main(String[] args) {
+       
+        //p1.Complex c1 = new p1.Complex();
+        Complex c1 = new Complex();
+        System.out.println(c1.toString());
+    }
+}
