d03b1e0202a8fc0eafcc6474368552403585b973
porjesuraj <porjesuraj5@gmail.com>
2020-10-30 15:26:17 +0530

    Day 5 'classwork folder 5.14 demo on array of primitive type , done'

---
+++ Classwork/Day5_30_Oct/Day_5.14/src/test/Program.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.util.Arrays;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		double[] arr = new double[3]; //array of primitive type
+		
+		System.out.println(Arrays.toString(arr));//
+	}
+public static void main2(String[] args) {
+	
+		int[] arr = new int[3]; //array of primitive type
+		
+		System.out.println(Arrays.toString(arr));//
+	}
+	public static void main1(String[] args) {
+		
+		
+		boolean[] arr = new boolean[3]; //array of primitive type
+		
+		System.out.println(Arrays.toString(arr));//[false, false, false]
+		
+		
+		
+	}
+
+}
