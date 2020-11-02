5df7f03ac3875f56e9f5ecd3318feedfa34be602
porjesuraj <porjesuraj5@gmail.com>
2020-11-02 16:13:07 +0530

    Classwork Day-6 'folder 6.8 demo updated to show private member of super class can be accessed in sub class'

--- Classwork/Day6_02_Nov/Day_6.8/src/test/Program.java
+++ Classwork/Day6_02_Nov/Day_6.8/src/test/Program.java
@@ -4,43 +4,71 @@ import java.lang.reflect.Field;
 class Person{
 	private String name;
 	private int age;
-	
+	public Person() {
+	}
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
-	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
-class Employee extends Person{
+class Employee //extends Person
+{
 	private int empid;
 	private float salary;
+	public Employee() {
+	}
 	
-	public Employee(String name, int age, int empid, float salary){
-		super( name, age );
+	public Employee(String name, int age, int empid, float salary) {
+		//super(name,age);
 		this.empid = empid;
 		this.salary = salary;
 	}
-	
+
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public void printRecord( ) throws Exception{
+		Class<?> c = this.getClass().getSuperclass();
+		
+		Field field = c.getDeclaredField("name");
+		field.setAccessible(true);
+		System.out.println("Name	:	"+field.get(this));
+		
+		field = c.getDeclaredField("age");
+		field.setAccessible(true);
+		System.out.println("Age	:	"+field.getInt(this));
+		
+		System.out.println("Empid	:	"+this.empid);
+		System.out.println("Salary	:	"+this.salary);
+	}
 }
 public class Program {
-	public static void main(String[] args) throws NoSuchFieldException, SecurityException {
-		
-		Employee emp = new Employee("Suraj", 26, 1, 1000.5f);
-		
-		/*
-		 * Class<?> c = emp.getClass();
-		 * 
-		 * Field field = null;
-		 * 
-		 * field = c.getField("name");
-		 * 
-		 * System.out.println();
-		 */
+	public static void main(String[] args) throws Exception{
+		Employee emp = new Employee("Sandeep",36,33,45000.0f);
+		emp.printRecord();
 		
 		
-		
-	}
-
+	}		
+	
 }
-
-            
