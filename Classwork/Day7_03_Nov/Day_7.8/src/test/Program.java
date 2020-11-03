4481284e9b279576079284216dba9cb3d1c86634
porjesuraj <porjesuraj5@gmail.com>
2020-11-03 16:57:49 +0530

    Classwork day 7 'folder 7.8 demo on unchecked and checked exception , and need for try catch in case of checke'

--- Classwork/Day7_03_Nov/Day_7.8/src/test/Program.java
+++ Classwork/Day7_03_Nov/Day_7.8/src/test/Program.java
@@ -1,7 +1,8 @@
 package test;
 
 public class Program {
-	public static void showRecord( ) {
+	public static void main(String[] args) {
+		//public static void sleep(long millis) throws InterruptedException
 		try {
 			for( int count = 1; count <= 10; ++ count ) {
 				System.out.println("Count	:	"+count);
@@ -11,19 +12,10 @@ public class Program {
 			e.printStackTrace();
 		}
 	}
-	public static void displayRecord( ) throws InterruptedException {
-		for( int count = 1; count <= 10; ++ count ) {
-			System.out.println("Count	:	"+count);
-			Thread.sleep(250);
-		}
-	}
-	public static void main(String[] args){
-		//Program.showRecord();
-		
-		try {
-			Program.displayRecord();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	public static void main1(String[] args){
+		//public static int parseInt(String s)throws NumberFormatException
+		String str = "125";
+		int number = Integer.parseInt(str); //OK
+		System.out.println("Number	:	"+number);
 	}
 }
