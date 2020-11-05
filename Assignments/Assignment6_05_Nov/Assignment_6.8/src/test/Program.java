79c0ba2927e9046fcaf09617bb9cac6497b66dfc
porjesuraj <porjesuraj5@gmail.com>
2020-11-05 21:58:59 +0530

    Assignment 6.8

---
+++ Assignments/Assignment6_05_Nov/Assignment_6.8/src/test/Program.java
@@ -0,0 +1,60 @@
+package test;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Program {
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+	
+		
+		System.out.println("please input a sentence ");
+		
+		String delim = " ";
+		
+		StringTokenizer stk = new StringTokenizer(sc.nextLine(),delim,false );
+		
+		String[] token  = new String[stk.countTokens()];
+		int i = 0;
+	while(stk.hasMoreElements())
+	{
+		token[i] = stk.nextToken();
+		
+		
+		
+		i++;
+	}
+	
+	for(String t : token)
+	{
+		StringBuilder sb = new StringBuilder(t);
+		
+		if(sb.charAt(sb.length() - 1) == 'D' && sb.charAt(sb.length() - 2) == 'E' )
+		  System.out.print(" " +  sb.toString() + " ");  
+	}
+	
+	System.out.println();
+	
+	
+	}
+	 
+
+}
+
+
+/*8. (Tokenizing and Comparing Strings) Write an application that
+reads a line of text, tokenizes it using space characters as
+delimiters and outputs only those words ending with the letters
+"ED".*/
+
+
+
+
+
+
+
+
+
+
