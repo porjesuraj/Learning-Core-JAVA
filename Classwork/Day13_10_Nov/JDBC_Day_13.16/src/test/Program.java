ff4e09f477b523ecf39873f741d4cd0764642895
porjesuraj <porjesuraj5@gmail.com>
2020-11-10 20:41:59 +0530

    classwork day13 'demo 9 JDBC ,parameterized query'

--- Classwork/Day13_10_Nov/JDBC_Day_13.16/src/test/Program.java
+++ Classwork/Day13_10_Nov/JDBC_Day_13.16/src/test/Program.java
@@ -1,6 +1,4 @@
-               
 package test;
-
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -12,25 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import pojo.Book;
-
 import utils.DbUtils;
 
+//statement : compile each time , cant handle special character
 public class Program {
 	public static void main(String[] args) {
-		//String sql = "INSERT INTO books (book_id, subject_name, book_name, author_name, price ) VALUES(?,?,?,?,?)";	//Parameterized query => ? IN parameter placeholders
-		String sql = "INSERT INTO books VALUES(?,?,?,?,?)";	//Parameterized query => ? IN parameter placeholders
+		//String sql = "INSERT INTO books (book_id, subject_name, book_name, author_name, price ) VALUES(?,?,?,?,?)";	//Parameterized query => ? IN parameter placeholders		
+		String sql = "insert into books values(?,?,?,?,?)"; 
 		try (Connection connection = DbUtils.getConnection(); 
-			 PreparedStatement statement = connection.prepareStatement(sql);) {
-			int bookId = 1027;
+				PreparedStatement statement = connection.prepareStatement(sql);) {
+			int bookId = 1029;
 			String subjectName = "OS", bookName = "Operating System Concept's", authorName = "Galvin";
 			float price = 575.50f;
 			
 			statement.setInt(1, bookId);
-			statement.setString(2, subjectName );
-			statement.setString(3, bookName );
-			statement.setString(4, authorName );
-			statement.setFloat(5, price);
-			
+	        statement.setString(2, subjectName);
+	        statement.setString(3, bookName);
+	        statement.setString(4, authorName);
+	        statement.setFloat(5, price);
 			int count = statement.executeUpdate();
 			System.out.println(count+" row(s) affected." );
 
@@ -39,3 +36,5 @@ public class Program {
 		}
 	}
 }
+
+            
